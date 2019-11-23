45eb5a3405e8060d419a2fbf4f0b617a02c03390
wwsalmon <wwsamson@yahoo.com>
2019-11-22 22:14:54 -0500

    Basic scrolling behavior

--- mirrored.js
+++ mirrored.js
@@ -1,2 +1,12 @@
 scriptIn = window.opener.scriptIn;
 $("#script").text(scriptIn);
+
+window.onresize = function() {
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+    window.opener.window.resizeTo(windowWidth,windowHeight);
+}
+
+// window.onscroll = function() {
+//     console.log(window.scrollY, window.scrollTop);
+// }
--- prompter.js
+++ prompter.js
@@ -1,8 +1,17 @@
 scriptIn = window.opener.scriptContent;
 $("#script").text(scriptIn);
-m = window.opener.m;
-console.log(m);
 
 $("#openmirror").on("click",function(){
-    m = window.open('mirrored.html');
-})
+    m = window.open('mirrored.html', '', 'menubar=no');
+    windowWidth = window.innerWidth;
+    windowHeight = window.innerHeight;
+    m.window.resizeTo(windowWidth, windowHeight);
+});
+
+window.onscroll = function () {
+    topscroll = window.scrollY;
+    m.document.body.scrollTop = topscroll;
+    // m.window.scrollY = topscroll;
+    m.window.scrollTo(0, topscroll);
+    // console.log(m.window.scrollY, m.document.body.scrollTop, topscroll)
+}
